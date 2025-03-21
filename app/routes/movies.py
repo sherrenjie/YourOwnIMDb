d9e2089f7ec866df423d35e4ad106d23a8f17624
sherrenjie <jiesherren@gmail.com>
2025-03-21 17:28:11 -0400

    Update movies.py

--- app/routes/movies.py
+++ app/routes/movies.py
@@ -43,7 +43,10 @@ def like_movie():
     #    - Why we pass `params` as a tuple (movie_id, user_email).
     #    - The role of `commit=True` in saving changes.
 
-    query = """ """
+    query = """
+    INSERT INTO Likes (mpid, uemail) VALUES (%s, %s);
+    """  # This query inserts a like record into the 'Likes' table.
+
 
     with Database() as db:
         try:
